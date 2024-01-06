51af812470039388d25767e1689ad1a17923af5e
Ishan Mani Subedi <ishansubedi7@gmail.com>
2024-01-06 10:02:38 +0545

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author='Naegle Lab',
     url='https://github.com/NaegleLab/OpenEnsembles',
     packages=['openensembles'],
-    install_requires=['pandas==1.2.*', 'numpy==1.19.*', 'scipy==1.6.*', 'networkx==2.5', 'matplotlib==3.3.*', 'hdbscan==0.8.*'],
+    install_requires=['networkx==2.5'],
     license='GNU General Public License v3',
     summary=DESC,
     long_description=DESC,
